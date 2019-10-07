495887a5453ae29cb1c834fc820bb9226cf44220
sandeepnegi1996 <9041128179sandy@gmail.com>
2019-10-07 22:18:30 +0530

    Working yes and no

--- createProject.sh
+++ createProject.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-echo "### Python Automation script...."
+echo " Python Automation scriptn ####### "
 
 read -p "Enter the Project Name : " projectName
 
@@ -9,25 +9,26 @@ read -p "Enter the Project Description : " description
 
 read -p "Enter the email adress: " userName
 
+# -s is used for stealth mode 
+
 read -s -p "Enter the password: " password
 
 echo "Entered details are...Project:  $projectName description: $description username: $userName "
-
-echo "#######  calling python script..... "
 
 #we are running python script with the command line arguments of project name , description,username,password
 #in the python script we are using selenium and chrome we driver to go to github.com and click on various links.. 
 
 read -p "Sure you want to create Repository press (y/n) ?" response
 
-if [ $response=='y' ]
+if [ $response == [yY] ]
 then
-	echo "Calling Python Script ..."
 	python pySeleniumTest.py $projectName $description $userName $password
 else
-	echo "Exiting..."
-	exit 0
+	echo "exiting..."
+	exit 1
 fi
+
+
 
 echo "End of Script..."
 
--- pySeleniumTest.py
+++ pySeleniumTest.py
@@ -2,7 +2,7 @@ import sys
 from selenium import webdriver
 driver = webdriver.Chrome("C:\\chromedriver.exe")
 
-printf (".........Starting the script..........")
+print("Starting the script...........")
 
 # taking the command line arguements and converting to string...
 
@@ -15,43 +15,43 @@ passwordUser=str(sys.argv[4])
 
 # this will hit the github.com my favorite place
 
-printf ("reached github.com...")
+print("reached github.com...")
 driver.get("https://www.github.com")
 
 # we are using xpath to point to particular location
 
 #clicking the sign in button
-printf ("clicking on sign in button...")
+print("clicking on sign in button...")
 driver.find_element_by_xpath("/html/body/div[1]/header/div/div[2]/div[2]/a[1]").click()
 
 # send_keys means we are going to write something
 # we are writing user name and password
 
-printf ("Entering user credentials...")
+print("Entering user credentials...")
 driver.find_element_by_xpath("/html/body/div[3]/main/div/form/div[3]/input[1]").send_keys(user)
 driver.find_element_by_xpath("/html/body/div[3]/main/div/form/div[3]/input[2]").send_keys(passwordUser)
 
 
 #clickint the login button
-printf ("logging in...")
+print("logging in...")
 driver.find_element_by_xpath("/html/body/div[3]/main/div/form/div[3]/input[8]").click()
 
 
 #clicking on creating a new repository
 
-printf ("creating a new repo...")
+print("creating a new repo...")
 driver.find_element_by_xpath("/html/body/div[4]/div/aside[1]/div[2]/div/div/h2/a").click()
 
 #entering the project name and description
 
-printf ("Entering project description..")
+print("Entering project description..")
 driver.find_element_by_xpath("/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input").send_keys(pName)
 
 #entering the description 
 driver.find_element_by_xpath("/html/body/div[4]/main/div/form/div[3]/dl/dd/input").send_keys(pDescription)
 
 #clicking the button of creating a repository
-driver.find_element_by_xpath("/html/body/div[4]/main/div/form/div[3]/button").click()
+driver.find_element_by_css_selector("#new_repository > div.js-with-permission-fields > button").click()
 
 #out git repository will be
 
